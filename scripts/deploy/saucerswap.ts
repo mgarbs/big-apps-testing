cca9e57077eaad9b06f45ca1fb3bc6b3fa49d6d3
mgarbs <michael.garber@hashgraph.com>
2025-08-27 15:38:02 -0400

    update deployment param in factory test (expected in tinycents)
    Signed-off-by: mgarbs <michael.garber@hashgraph.com>

--- scripts/deploy/saucerswap.ts
+++ scripts/deploy/saucerswap.ts
@@ -35,8 +35,8 @@ export class SaucerSwapDeployer extends Deployer {
     const Factory = await ethers.getContractFactory("UniswapV2Factory");
     deployments.factory = await Factory.deploy(
       deployer.address,
-      ethers.parseEther("1"),
-      ethers.parseEther("10")
+      100, // 1 USD worth of tinycents
+      200 // 2 USD worth of tinycents
     );
     await deployments.factory.waitForDeployment();
     this.logger.success(`Factory deployed to: ${await deployments.factory.getAddress()}`);
